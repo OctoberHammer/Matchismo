d79ea90dc7c8f62778593eb581f1fbdd7a94a509
Ocotober Hammer <octoberhammer@Ocotobers-MacBook-Pro.local>
2015-05-08 08:19:25 +0300

    Initial Commit

---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Ocotober Hammer on 5/8/15.
+//  Copyright (c) 2015 Ocotober Hammer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Matchismo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Matchismo
+//
+//  Created by Ocotober Hammer on 5/8/15.
+//  Copyright (c) 2015 Ocotober Hammer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
